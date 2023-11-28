4b5c4a5a40c1f0f87732dc1eed220bbc1da55220
OppSpark <picks5@naver.com>
2023-11-28 13:38:21 +0900

    camera 1, 2 merage

--- libs/keyboardEvent.js
+++ libs/keyboardEvent.js
@@ -2,7 +2,6 @@ export class KeyboardBuilder{
     constructor(){
         this.input = {};
     }
-
     setKey(input, content ){
         this.input[input] = content;
         return this;
@@ -16,41 +15,4 @@ export class KeyboardBuilder{
         });
         return this;
     }
-
 }
-
-
-
-
-
-/*
-function animate() {
-                requestAnimationFrame(animate);
-
-                // 플레이어 이동 로직
-                if (isKeyPressed('ArrowLeft')) cube.position.x -= 0.1;
-                if (isKeyPressed('ArrowRight')) cube.position.x += 0.1;
-                if (isKeyPressed('ArrowUp')) cube.position.y += 0.1;
-                if (isKeyPressed('ArrowDown')) cube.position.y -= 0.1;
-
-                // 렌더링
-                renderer.render(scene, camera);
-            }
-
-            // 키보드 입력 관련 변수 및 이벤트 리스너
-            const keyStates = {};
-            window.addEventListener('keydown', (event) => {
-                keyStates[event.code] = true;
-            });
-            window.addEventListener('keyup', (event) => {
-                keyStates[event.code] = false;
-            });
-
-            // 키보드 입력 상태 확인 함수
-            function isKeyPressed(keyCode) {
-                return keyStates[keyCode] === true;
-            }
-
-            // 애니메이션 시작
-            animate();
-*/
